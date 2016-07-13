02e297515e877082c6eee56e23c6b3e554091462
l <l>
2016-07-13 19:04:45 +0800

    shiro env+restful token

---
+++ java/fansfunding/src/main/java/com/fansfunding/common/entity/TokenRealm.java
@@ -0,0 +1,56 @@
+package com.fansfunding.common.entity;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.fansfunding.common.interceptor.StatelessToken;
+import com.fansfunding.user.service.UserService;
+
+@Component("UserShiro")
+public class TokenRealm extends AuthorizingRealm{
+	
+	@Autowired
+	private UserService userService;
+	
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return (token instanceof StatelessToken)||(token instanceof UsernamePasswordToken);
+    }
+	
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		String username=(String)principals.getPrimaryPrincipal();
+		SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+		
+		//Subject subject = SecurityUtils.getSubject();
+		
+		//subject.checkRole("admin");
+		//TODO:NOT FINISHED!!!
+		authorizationInfo.setRoles(userService.findRoles(username));
+		authorizationInfo.setStringPermissions(userService.findPermissions(username));;
+		System.out.println(userService.findPermissions(username));
+		return authorizationInfo;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		
+		if(token instanceof StatelessToken){
+			int tokenId = (int)token.getPrincipal();
+			
+			return new SimpleAuthenticationInfo(tokenId,tokenId,getName());
+		}		
+		return null;
+
+	}
+
+}

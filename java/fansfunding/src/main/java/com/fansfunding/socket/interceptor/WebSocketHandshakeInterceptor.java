4bda65c9ec46ca9cfd721d87c633e99465257b28
wangle <048117wangle@gmail.com>
2016-07-27 01:13:30 +0800

    添加WebSocket支持

---
+++ java/fansfunding/src/main/java/com/fansfunding/socket/interceptor/WebSocketHandshakeInterceptor.java
@@ -0,0 +1,36 @@
+package com.fansfunding.socket.interceptor;
+
+import java.util.Map;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+/**
+ * 握手拦截，用于添加在线用户
+ * @author w_angler
+ *
+ */
+
+@Component
+public class WebSocketHandshakeInterceptor extends HttpSessionHandshakeInterceptor {
+
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request,
+			ServerHttpResponse response, WebSocketHandler wsHandler,
+			Map<String, Object> attributes) throws Exception {
+		System.out.println("Before Handshake");
+		return super.beforeHandshake(request, response, wsHandler, attributes);
+	}
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request,
+			ServerHttpResponse response, WebSocketHandler wsHandler,
+			Exception exception) {
+		System.out.println("After Handshake");
+		super.afterHandshake(request, response, wsHandler, exception);
+	}
+}

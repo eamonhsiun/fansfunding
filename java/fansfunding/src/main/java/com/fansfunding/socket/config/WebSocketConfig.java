1af74e1c742b48cfab75a0cee89e367c45abd5aa
wangle <048117wangle@gmail.com>
2016-07-28 00:55:25 +0800

    看了一下的spring-websocket的文档，修改了一下配置

--- java/fansfunding/src/main/java/com/fansfunding/socket/config/WebSocketConfig.java
+++ java/fansfunding/src/main/java/com/fansfunding/socket/config/WebSocketConfig.java
@@ -2,12 +2,11 @@ package com.fansfunding.socket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import com.fansfunding.socket.handler.TestHandler;
 import com.fansfunding.socket.interceptor.WebSocketHandshakeInterceptor;
@@ -21,9 +20,11 @@ import com.fansfunding.socket.interceptor.WebSocketHandshakeInterceptor;
  *
  */
 @Configuration
-@EnableWebMvc
 @EnableWebSocket
-public class WebSocketConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
+public class WebSocketConfig implements WebSocketConfigurer {
+	/**
+	 * 注册websocket处理器
+	 */
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(userHandler(),"/websocket/test").setAllowedOrigins("*")
@@ -32,9 +33,23 @@ public class WebSocketConfig extends WebMvcConfigurerAdapter implements WebSocke
         		.addInterceptors(new WebSocketHandshakeInterceptor()).withSockJS();
     }
 
+    /**
+     * 配置servlet容器
+     * @return
+     */
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(8192);
+        container.setMaxBinaryMessageBufferSize(8192);
+        return container;
+    }
+    /**
+     * 注入
+     * @return
+     */
     @Bean
     public WebSocketHandler userHandler(){
         return new TestHandler();
     }
-
 }

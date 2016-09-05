c8736547c41712eaba566ed06acda8c6a56102c4
wangle <048117wangle@gmail.com>
2016-09-06 00:09:14 +0800

    添加websocket的握手token验证

--- java/fansfunding/src/main/java/com/fansfunding/socket/handler/WebsocketHandshakeInterceptor.java
+++ java/fansfunding/src/main/java/com/fansfunding/socket/handler/WebsocketHandshakeInterceptor.java
@@ -4,12 +4,17 @@ import java.util.Map;
 import java.util.Objects;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import com.fansfunding.common.interceptor.StatelessAuthcFilter;
 
 /**
  * Http握手拦截器
@@ -19,6 +24,7 @@ import org.springframework.web.socket.server.HandshakeInterceptor;
 
 @Component
 public class WebsocketHandshakeInterceptor implements HandshakeInterceptor {
+	private static StatelessAuthcFilter filter=new StatelessAuthcFilter();
 	/**
 	 * websocket连接建立前的握手，验证参数合法性
 	 */
@@ -27,6 +33,7 @@ public class WebsocketHandshakeInterceptor implements HandshakeInterceptor {
 			ServerHttpResponse response, WebSocketHandler handler,
 			Map<String, Object> attrs) throws Exception {
 		HttpServletRequest httpRequest=((ServletServerHttpRequest)request).getServletRequest();
+		HttpServletResponse httpResponse=((ServletServerHttpResponse)response).getServletResponse();
 		//用户合法性验证
 		String userId =httpRequest.getParameter("userId");
 		if(Objects.isNull(userId)||(!userId.matches("\\d+"))){
@@ -34,7 +41,7 @@ public class WebsocketHandshakeInterceptor implements HandshakeInterceptor {
 		}
 		//TODO 验证token
 		String token = httpRequest.getParameter("token");
-		if(Objects.isNull(token)||(!validate(token))){
+		if(Objects.isNull(token)||(!filter.websocketFilter(httpRequest, httpResponse))){
 			return false;
 		}
 		attrs.put("userId", Integer.parseInt(userId));
@@ -45,13 +52,5 @@ public class WebsocketHandshakeInterceptor implements HandshakeInterceptor {
 	public void afterHandshake(ServerHttpRequest request,
 			ServerHttpResponse response, WebSocketHandler wsHandler,
 			Exception exception) {
-	}
-	/**
-	 * 验证token
-	 * @param token
-	 * @return
-	 */
-	private boolean validate(String token){
-		return true;
 	}
 }
